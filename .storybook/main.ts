8b7fb1c4cec9b4c61664973d001e37740a785c53
Amy Li <amyligaoge@gmail.com>
2026-06-23 14:29:29 -0700

    Reorganize files into directories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   "stories": [
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../src/components/**/stories/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [],
   "framework": "@storybook/react-vite"
